a1c05c14286c30d51b35334bd18fcf976215112d
StaffanHynge <>
2023-05-12 12:12:03 +0000

    add models and admin settings

--- songs/views.py
+++ songs/views.py
@@ -1,5 +1,7 @@
 from django.views.generic import TemplateView
 from django.shortcuts import render
+from .models import Music
 
 class Index(TemplateView):
+    music = Music.objects.all()
     template_name = 'home/index.html'
