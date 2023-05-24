c9601bf91f73efb9a0aa3cb1aeb6bb3a63502517
StaffanHynge <>
2023-05-24 16:34:20 +0000

    add song_detail page

--- songs/urls.py
+++ songs/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
-from .views import AddSong
+from .views import AddSong, MusicList, HomeView, SongDetail
 
 urlpatterns = [
-    path("", views.MusicList.as_view(), name="home"),
-    path("add_song/", AddSong.as_view(), name="add_song")
+    path("", HomeView.as_view(), name="home"),
+    path("add_song/", AddSong.as_view(), name="add_song"),
+    path("songs", MusicList.as_view(), name="songs"),
+    path("<slug:pk>/", SongDetail.as_view(), name="song_detail"),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,15 +1,15 @@
 from songs.models import Music
 from django.views import generic
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView, ListView, DetailView
 from django.shortcuts import redirect, render
 from .forms import MusicForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class MusicList(generic.ListView):
+class MusicList(ListView):
     model = Music
-    queryset = Music.objects.filter(status=1).order_by('-created_on')
-    template_name = 'home.html'
+    template_name = 'songs.html'
     paginate_by = 6
+    context_object_name = "music_list"
 
 class AddSong(LoginRequiredMixin, CreateView):
     # Add a song 
@@ -20,4 +20,20 @@ class AddSong(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.user = self.request.user
-        return super(AddSong, self).form.valid(form)
+        return super(AddSong, self).form_valid(form)
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+class SongDetail(DetailView):
+    model = Music
+    template_name = 'song_detail.html'
+    context_object_name = "song"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        song = self.get_object()
+        context['pk'] = song.pk
+        return context
+
+
