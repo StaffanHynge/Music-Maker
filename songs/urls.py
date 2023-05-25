44b66e5dda6a6cb9c3eba7625a6d5bfb044942f2
StaffanHynge <>
2023-05-25 09:06:29 +0000

    add edit function

--- songs/urls.py
+++ songs/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
-from .views import AddSong, MusicList, HomeView, SongDetail
+from .views import AddSong, MusicList, HomeView, SongDetail, DeleteSong, EditSong
 
 urlpatterns = [
     path("", HomeView.as_view(), name="home"),
     path("add_song/", AddSong.as_view(), name="add_song"),
     path("songs", MusicList.as_view(), name="songs"),
     path("<slug:pk>/", SongDetail.as_view(), name="song_detail"),
+    path("delete/<slug:pk>/", DeleteSong.as_view(), name="delete_song"),
+    path("edit/<slug:pk>/", EditSong.as_view(), name="edit_song"),
 ]
--- songs/views.py
+++ songs/views.py
@@ -1,9 +1,9 @@
 from songs.models import Music
 from django.views import generic
-from django.views.generic import CreateView, TemplateView, ListView, DetailView
+from django.views.generic import CreateView, TemplateView, ListView, DetailView, DeleteView, UpdateView
 from django.shortcuts import redirect, render
 from .forms import MusicForm
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class MusicList(ListView):
     model = Music
@@ -16,7 +16,7 @@ class AddSong(LoginRequiredMixin, CreateView):
     template_name = 'add_song.html'
     model = Music
     form_class = MusicForm
-    success_url = '/add_song/'
+    success_url = '/songs'
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -36,4 +36,19 @@ class SongDetail(DetailView):
         context['pk'] = song.pk
         return context
 
+class DeleteSong(LoginRequiredMixin, UserPassesTestMixin, DeleteView): 
+    model = Music
+    success_url = '/songs'
+    template_name = 'music_confirm_delete.html' 
 
+    def test_func(self): 
+        return self.request.user == self.get_object().user
+
+class EditSong(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = 'edit_song.html'
+    model = Music
+    form_class = MusicForm
+    success_url = '/songs'
+
+    def test_func(self): 
+        return self.request.user == self.get_object().user
